8c52b9b955d28cdc9acdbaeb292820b8925a1507
Janet Scanlon <janet.Lscanlon@gmail.com>
2024-03-30 13:39:22 -0500

    select first name value working

--- javascript.js
+++ javascript.js
@@ -1,7 +1,13 @@
 console.log('JS is working!');
 
+
+/**
+ * A function for when the submit button is clicked
+ */
 function submitClicked(event){
+    //prevent the default behavior within the form
     event.preventDefault();
 
-    console.log('you pushed the button');
+    let newFirstName = document.getElementById('firstName-text').value;
+    console.log('this is the new First Name', newFirstName);
 }
