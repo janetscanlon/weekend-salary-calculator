92256f4f6f0cefe141d082296c3e227173bd37a9
Janet Scanlon <janet.Lscanlon@gmail.com>
2024-03-30 13:59:31 -0500

    new <tr> in js working with new values

--- javascript.js
+++ javascript.js
@@ -8,12 +8,24 @@ function submitClicked(event){
     //prevent the default behavior within the form
     event.preventDefault();
 
+    //select all of the new input values and create new variables for them
     let newFirstName = document.getElementById('firstName-text').value;
     let newLastName = document.getElementById('lastName-text').value;
     let newId = document.getElementById('id-text').value;
-    let newtitle = document.getElementById('title-text').value;
+    let newTitle = document.getElementById('title-text').value;
     let newAnnualSalary = document.getElementById('annualSalary-text').value;
 
-    console.log('all of the new values are:', newFirstName, newLastName, newId, newtitle,newAnnualSalary)
+
+    //create a variable for the new table row html 
+    let newEmployeeInfo =  `<td>${newFirstName}</td>
+                            <td>${newLastName}</td>
+                            <td>${newId}</td>
+                            <td>${newTitle}</td>
+                            <td>${newAnnualSalary}</td>
+                            <td><button data-testid="deleteButton"> Delete
+                            </button></td>;`
+
+    console.log('all of the new values are:', newFirstName, newLastName, newId, newTitle,newAnnualSalary);
+    console.log('test for the html with all the new inputs', newEmployeeInfo);
     
 }
